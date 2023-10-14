9c0fbb1291b165ebe819a61f4a39c773d3511607
Amitedke <103561128+Amitedke@users.noreply.github.com>
2023-10-14 14:47:35 +0530

    last commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Amit Edke",
+    author_email="amitedke@gmail.com",
+    packages=find_packages()
+)
